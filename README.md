76692797ba5b8f3b33b8f709983ce6102c1701da
sombii <blackknightstd@gmail.com>
2021-01-24 09:20:15 +0545

    added author credits

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MeroShare Clone Frontend (MERN)
 
+**This project is still a work in progress.**
+
 A small project to learn MERN stack by creating a clone of [Meroshare](https:///meroshare.cdsc.com.np).
 
-[Backend Repository Link]() (Using Material UI)
+[Backend Repository Link](#) (Using Material UI)
--- src/components/dash/home/Dashboard.js
+++ src/components/dash/home/Dashboard.js
@@ -1,14 +1,17 @@
-import {Link} from "react-router-dom";
-import routes from "../../../constants/routes";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
 import CardContent from "@material-ui/core/CardContent";
-import {CardActions} from "@material-ui/core";
+import {CardActions, Icon, Paper} from "@material-ui/core";
 import DataTable from "react-data-table-component";
 import ThemedButton from "../../shared/ThemedButton";
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles(() => ({
     cardRoot: {
@@ -27,6 +30,15 @@ const useStyles = makeStyles(() => ({
     },
     iconRoot: {
         color: "whitesmoke",
+    },
+    author: {
+        backgroundColor: "#57606b",
+        padding: "1rem",
+        marginBottom: "1rem",
+    },
+    authorPaper: {
+        backgroundColor: "#57606b",
+        color: "whitesmoke",
     }
 }));
 
@@ -40,6 +52,32 @@ const Dashboard = () => {
 
     return (
         <Grid>
+            <Grid item container className={classes.author}>
+                <Grid item xs={12} sm={6}>
+                    <Paper elevation={0} square className={classes.authorPaper}>
+                        <Typography variant="h6" component="p">MeroShare Clone</Typography>
+                        <Typography variant="body2" component="p">This is a clone app of Meroshare, written in
+                            React/Material UI, opensource (MIT). Find the
+                            source in the github repo linked below.</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Paper elevation={0} square className={classes.authorPaper}>
+                        {/*<AccountCircleOutlinedIcon style={{fontSize: "4rem"}}/>*/}
+                        <Typography component="p" variant="h6">Author: <strong>Sanjog Rai</strong></Typography>
+                        <Link href="http://github.com/sombii" target="_blank">
+                            <IconButton>
+                                <GitHubIcon style={{color: "skyblue"}}/>
+                            </IconButton>
+                        </Link>
+                        <Link href="https://www.linkedin.com/in/sombii" target="_blank">
+                            <IconButton>
+                                <LinkedInIcon style={{color: "skyblue"}}/>
+                            </IconButton>
+                        </Link>
+                    </Paper>
+                </Grid>
+            </Grid>
             <Grid item container spacing={3}>
                 <Grid item xs sm md={6}>
                     <Card elevation={0} classes={{root: classes.cardRoot}}>
@@ -73,7 +111,7 @@ const Dashboard = () => {
             <Grid item container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
                     <DataTable columns={[{name: "Scrip", selector: "scrip"}, {name: "Share Value", selector: "value"}]}
-                               data={[{scrip: "NRIC", value: "80,000"},{scrip: "GIC", value: "10,230"},]}
+                               data={[{scrip: "NRIC", value: "80,000"}, {scrip: "GIC", value: "10,230"},]}
                                title="Top Shares Value" actions={actions}/>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
